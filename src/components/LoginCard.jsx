0de64244860c403c5af91b266c07c0a2f2c16aeb
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-12 22:34:57 +0530

    Update LoginCard.jsx

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -4,7 +4,7 @@ import { EvervaultCard, Icon } from "./ui/evervault-card";
 export function EvervaultCardDemo() {
   return (
     (<div
-      className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col top-20 items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+      className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col top-20 items-start max-w-sm mx-auto p-4 relative h-[30rem] overflow-x-hidden">
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
