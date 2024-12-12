b3caf491788f046f6858f6ff6dba920eccb64ae8
ishan-26 <git21cs071-t@students.git.edu>
2024-12-12 22:29:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import LoginButtonsDemo from './components/LoginCard'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-black h-screen overflow-x-hidden">
+    <LoginButtonsDemo/>
+    </div>
+    </>
+  )
+}
+
+export default App
