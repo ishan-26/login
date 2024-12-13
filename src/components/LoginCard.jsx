bd432466e0723ec379544e0382d6a1e528d7344e
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-13 16:16:42 +0530

    Update LoginCard.jsx

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { EvervaultCard, Icon } from "./ui/evervault-card";
+import loginbg from "../assets/loginbg.jpg";
 
 export function EvervaultCardDemo() {
   return (
-    <div
-      className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col top-20 items-start max-w-sm mx-auto p-4 relative h-[30rem] overflow-hidden"
-    >
-      <Icon className="absolute h-6 w-6 top-2 left-2 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 bottom-2 left-2 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 top-2 right-2 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 bottom-2 right-2 dark:text-white text-black" />
-      <EvervaultCard />
-    </div>
+    <>
+      <img
+        src={loginbg}
+        alt="Demo Background"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+
+      <div className="relative flex items-center justify-center h-screen"> {/* Center the content */}
+        <div className="relative bg-black bg-opacity-90 border border-black/[0.2] dark:border-white/[0.2] rounded-lg w-full max-w-sm p-4 h-[30rem]"> {/* Card Container */}
+          {/* Icons */}
+          <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-white" />
+          <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-white" />
+          <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-white" />
+          <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-white" />
+
+          {/* Evervault Card */}
+          <EvervaultCard />
+        </div>
+      </div>
+    </>
   );
 }
 
