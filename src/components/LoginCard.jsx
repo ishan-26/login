51f87651048d7797e8f414230cc3c5e2838dbf0e
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-13 17:43:53 +0530

    overflow-hidden

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -8,7 +8,7 @@ export function EvervaultCardDemo() {
       <img
         src={loginbg}
         alt="Demo Background"
-        className="absolute inset-0 w-full h-full object-cover"
+        className="absolute inset-0 w-full h-full object-cover overflow-hidden"
       />
 
       <div className="relative flex items-center justify-center h-screen"> {/* Center the content */}
