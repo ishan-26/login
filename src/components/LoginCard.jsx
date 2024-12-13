4b6e5b34aefd6578d7ad2997e156f31ddc0365eb
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-12-13 10:14:46 +0530

    Update LoginCard.jsx

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -3,16 +3,15 @@ import { EvervaultCard, Icon } from "./ui/evervault-card";
 
 export function EvervaultCardDemo() {
   return (
-    (<div
-      className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col top-20 items-start max-w-sm mx-auto p-4 relative h-[30rem] overflow-x-hidden">
-      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-      <Icon
-        className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+    <div
+      className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col top-20 items-start max-w-sm mx-auto p-4 relative h-[30rem] overflow-hidden"
+    >
+      <Icon className="absolute h-6 w-6 top-2 left-2 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 bottom-2 left-2 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 top-2 right-2 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 bottom-2 right-2 dark:text-white text-black" />
       <EvervaultCard />
-      
-    </div>)
+    </div>
   );
 }
 
